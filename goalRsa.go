32354259889deb8d98fd1f741e227349525ce479
Hari-Kiri <pratamahari89@gmail.com>
2022-07-05 15:07:37 +0000

    Change error variable

--- goalRsa.go
+++ goalRsa.go
@@ -6,9 +6,9 @@ import (
 )
 
 func NewRsaKeyPair(bits int) (*rsa.PrivateKey, *rsa.PublicKey, error) {
-	privateKey, errorGeneration := rsa.GenerateKey(rand.Reader, bits)
-	if errorGeneration != nil {
-		return nil, nil, errorGeneration
+	privateKey, error := rsa.GenerateKey(rand.Reader, bits)
+	if error != nil {
+		return nil, nil, error
 	}
 	return privateKey, &privateKey.PublicKey, nil
 }
